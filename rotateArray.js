83a6bff14b49e76bc9a522a9d7d9a8301065f5e5
Rafael <rafael@rafaelespinoza.com>
2014-04-27 23:54:20 -0700

    modify rotate array method to not modify original array and to rotate a set number of times

--- rotateArray.js
+++ rotateArray.js
@@ -1,7 +1,8 @@
+// modifies the original array. moves it by 1 slot
 // @param direction: 
 //   (-1) if we are moving the last element to become the first element 
 //   (0)  if we are moving the first element to become the last element
-var rotate = function(arr, direction) {
+var rotateSelf = function(arr, direction) {
   if (direction != 0 && direction != -1) {
     direction = -1;
   }
@@ -11,7 +12,26 @@ var rotate = function(arr, direction) {
 var myArr = ['a','b','c','d','e','f','g','h','i','j'];
 var arr = ['a', 'b', 'c'];
 console.log(arr);
-rotate(arr, 0);
+rotateSelf(arr, 0);
 console.log(arr);
-rotate(arr, -1);
+rotateSelf(arr, -1);
 console.log(arr);
+
+// returns new copy of an array. modifies it a set number of times
+// @param times: number of times to move each letter. default set to 1 time.
+// @param direction: 
+//   (-1) if we are moving the last element to become the first element 
+//   (0)  if we are moving the first element to become the last element
+var rotate = function(arr, direction, times) {
+  if (direction != 0 && direction != -1) {
+    direction = -1;
+  }
+  times = typeof times === 'undefined' ? 1 : times; // set default value of times
+  var rotated = arr.map(function(val) {
+    return val;
+  });
+  for (var i = 1; i <= times; i++) {
+    direction == 0 ? rotated.push(rotated.shift(rotated[0])) : rotated.unshift(rotated.pop(rotated[0]));
+  }
+  return rotated;
+};
